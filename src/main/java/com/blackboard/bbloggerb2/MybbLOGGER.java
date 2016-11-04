b659fb405cf43cdace579afe757bce9328d91176
Mark Bykerk Kauffman <mark.kauffman@blackboard.com>
2016-11-04 11:05:17 -0700

    cleand up jsp comments

--- src/main/java/com/blackboard/bbloggerb2/MybbLOGGER.java
+++ src/main/java/com/blackboard/bbloggerb2/MybbLOGGER.java
@@ -1,4 +1,31 @@
 package com.blackboard.bbloggerb2;
+/* Copyright (C) 2016, Blackboard Inc.
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  -- Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *  -- Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *  -- Neither the name of Blackboard Inc. nor the names of its contributors 
+ *     may be used to endorse or promote products derived from this 
+ *     software without specific prior written permission.
+ *  
+ * THIS SOFTWARE IS PROVIDED BY BLACKBOARD INC ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL BLACKBOARD INC. BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 import blackboard.platform.log.Log;
 import blackboard.platform.log.LogService;
@@ -20,7 +47,7 @@ public class MybbLOGGER
     
   private static final Logger logback = LoggerFactory.getLogger(MybbLOGGER.class);  
   
-  private static MybbLOGGER DCBBLOGGER = null;
+  private static MybbLOGGER ABBLOGGER = null;
   private static LogService LOGGER = null;
   private static Log B2LOG = null;  // MBK - added so we can only create the log file when it doesn't exist.
 
@@ -53,14 +80,14 @@ public class MybbLOGGER
     }
   }
   
-  public static MybbLOGGER getDcbbLogger()
+  public static MybbLOGGER getBbLogger()
   {
-    if (DCBBLOGGER == null)
+    if (ABBLOGGER == null)
     {
-      DCBBLOGGER = new MybbLOGGER();
+      ABBLOGGER = new MybbLOGGER();
       LOGGER = LogServiceFactory.getInstance();
     }
-    return DCBBLOGGER;
+    return ABBLOGGER;
   }
   
   public void logError(String logMessage)
